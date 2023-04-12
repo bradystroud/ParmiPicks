aeee6fb188f2d5776e67523b67d7f9d3b366e3b2
Brady Stroud <bradystroud@ssw.com.au>
2023-04-13 07:09:05 +1000

    ✨  Add reviews page

---
+++ pages/reviews.tsx
@@ -0,0 +1,33 @@
+import { Container } from "../components/util/container";
+import { Section } from "../components/util/section";
+import { client } from "../tina/__generated__/client";
+import { Layout } from "../components/layout";
+import { Reviews } from "../components/posts/reviews";
+
+export default function ReviewPage(
+  props: AsyncReturnType<typeof getStaticProps>["props"]
+) {
+  const reviews = props.data.reviewConnection.edges;
+
+  return (
+    <Layout>
+      <Section className="flex-1">
+        <Container size="large" width="small">
+          <Reviews data={reviews} />
+        </Container>
+      </Section>
+    </Layout>
+  );
+}
+
+export const getStaticProps = async () => {
+  const tinaProps = await client.queries.reviewPageQuery();
+  return {
+    props: {
+      ...tinaProps,
+    },
+  };
+};
+
+export type AsyncReturnType<T extends (...args: any) => Promise<any>> =
+  T extends (...args: any) => Promise<infer R> ? R : any;

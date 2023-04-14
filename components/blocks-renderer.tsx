7c56604ea06b68cc492c00fdfad3612018c776a7
Brady Stroud <bradystroud@ssw.com.au>
2023-04-14 20:50:27 +1000

    wip - still broken

--- components/blocks-renderer.tsx
+++ components/blocks-renderer.tsx
@@ -10,47 +10,47 @@ export const Blocks = (props: Omit<Page, "id" | "_sys" | "_values">) => {
     <>
       {props.blocks
         ? props.blocks.map(function (block, i) {
-          switch (block.__typename) {
-            case "PageBlocksContent":
-              return (
-                <div
-                  data-tinafield={`blocks.${i}`}
-                  key={i + block.__typename}
-                >
-                  <Content data={block} parentField={`blocks.${i}`} />
-                </div>
-              );
-            case "PageBlocksHero":
-              return (
-                <div
-                  data-tinafield={`blocks.${i}`}
-                  key={i + block.__typename}
-                >
-                  <Hero data={block} parentField={`blocks.${i}`} />
-                </div>
-              );
-            case "PageBlocksFeatures":
-              return (
-                <div
-                  data-tinafield={`blocks.${i}`}
-                  key={i + block.__typename}
-                >
-                  <Features data={block} parentField={`blocks.${i}`} />
-                </div>
-              );
-            case "PageBlocksTestimonial":
-              return (
-                <div
-                  data-tinafield={`blocks.${i}`}
-                  key={i + block.__typename}
-                >
-                  <Testimonial data={block} parentField={`blocks.${i}`} />
-                </div>
-              );
-            default:
-              return null;
-          }
-        })
+            switch (block.__typename) {
+              case "PageBlocksContent":
+                return (
+                  <div
+                    data-tinafield={`blocks.${i}`}
+                    key={i + block.__typename}
+                  >
+                    <Content data={block} parentField={`blocks.${i}`} />
+                  </div>
+                );
+              case "PageBlocksHero":
+                return (
+                  <div
+                    data-tinafield={`blocks.${i}`}
+                    key={i + block.__typename}
+                  >
+                    <Hero data={block} parentField={`blocks.${i}`} />
+                  </div>
+                );
+              case "PageBlocksFeatures":
+                return (
+                  <div
+                    data-tinafield={`blocks.${i}`}
+                    key={i + block.__typename}
+                  >
+                    <Features data={block} parentField={`blocks.${i}`} />
+                  </div>
+                );
+              case "PageBlocksTestimonial":
+                return (
+                  <div
+                    data-tinafield={`blocks.${i}`}
+                    key={i + block.__typename}
+                  >
+                    <Testimonial data={block} parentField={`blocks.${i}`} />
+                  </div>
+                );
+              default:
+                return null;
+            }
+          })
         : null}
     </>
   );

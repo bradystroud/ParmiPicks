f744e3e310ec67d2b2cd8e48fd9f2848c063e2ba
Brady Stroud [SSW] <bradystroud@ssw.com.au>
2024-04-19 21:22:46 +0200

    Try fix images again

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "parmipicks.com",
+        hostname: "assets.tina.io",
         port: "",
       },
     ],

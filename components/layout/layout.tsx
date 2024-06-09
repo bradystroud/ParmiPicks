0de611c70b6dcd3df26aac3421455495a832d73b
Brady Stroud [SSW] <bradystroud@ssw.com.au>
2024-06-09 15:48:20 +0930

    Add twitter OG stuff

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -6,6 +6,11 @@ import layoutData from "../../content/global/index.json";
 import NextBreadcrumb from "./breadcrumb";
 
 export const Layout = ({ data = layoutData, children }) => {
+  //TODO: These vales should not be hardcoded - Move to TinaCMS
+  const ogImageUrl =
+    "https://assets.tina.io/c68a0182-b88f-4a74-8514-0cbe71f98577/nice-parm.jpg";
+  const ogDescription =
+    "Reviews of every parmi I've tried and reviews. Browse through my reviews to find the best";
   return (
     <>
       <Head>
@@ -15,17 +20,14 @@ export const Layout = ({ data = layoutData, children }) => {
           content="Chicken parmi reviews - my personal list of every parmi I've tried and rated. Browse through my reviews to find the best"
           key="desc"
         />
+        <meta property="twitter:title" content="Parmi Picks" />
         <meta property="og:site_name" content="Parmi Picks" />
         <meta property="og:url" content="https://parmipicks.com/" />
         <meta property="og:type" content="website" />
-        <meta
-          property="og:description"
-          content="Reviews of every parmi I've tried and reviews. Browse through my reviews to find the best"
-        />
-        <meta
-          property="og:image"
-          content="https://assets.tina.io/c68a0182-b88f-4a74-8514-0cbe71f98577/nice-parm.jpg"
-        />
+        <meta property="og:description" content={ogDescription} />
+        <meta property="twitter:description" content={ogDescription} />
+        <meta property="twitter:image" content={ogImageUrl}></meta>
+        <meta property="og:image" content={ogImageUrl} />
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍴</text></svg>"
